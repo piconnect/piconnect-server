e61352f7f388f4802a5cc4d45b801e5862e3d5fa
Anuj Verma <anuj.verma@ebizontek.com>
2017-05-29 19:52:05 +0530

    setup complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pi works!';
+  title = "Setup completed, Now it's begain";
 }
