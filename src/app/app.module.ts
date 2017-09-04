a1421979771a2c6fd0ea6c9a261c9f2ec2f4c693
Anuj Verma <anuj.verma@ebizontek.com>
2017-09-04 18:51:43 +0530

    not-found component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignedInComponent } from './signed-in/signed-in.component';
 import { SignedOutComponent } from './signed-out/signed-out.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignedInComponent,
-    SignedOutComponent
+    SignedOutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
