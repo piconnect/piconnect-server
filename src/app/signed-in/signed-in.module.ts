f9d5f3c20ab7f0c34f3b37b9b02d29fb63317bf3
Anuj Verma <anuj.verma@ebizontek.com>
2017-09-05 11:20:33 +0530

    modules added

---
+++ src/app/signed-in/signed-in.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SignedInRoutingModule } from './signed-in-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SignedInRoutingModule
+  ],
+  declarations: []
+})
+export class SignedInModule { }
---
+++ src/app/signed-out/signed-out.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SignedOutRoutingModule } from './signed-out-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SignedOutRoutingModule
+  ],
+  declarations: []
+})
+export class SignedOutModule { }
