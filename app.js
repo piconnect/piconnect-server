d31cd76b486f8edb5d78776821638a63e8a89503
Anuj Verma <anuj.verma@ebizontek.com>
2017-09-06 17:10:38 +0530

    default routing added in express

--- app.js
+++ app.js
@@ -44,29 +44,8 @@ app.use('/api/v1', devApi);
 app.use('/api', api);
 app.use( "*" , angularApp );
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 app.use(function (err, req, res, next) {
-  if (err.name === 'UnauthorizedError') {
-    res.status(401);
-    res.json({"message" : err.name + ": " + err.message});
-  }
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.sendfile( path.join( __dirname, "dist/index.html" ));
 });
 
 module.exports = app;
