dd17fdb4aacac3da9ed51c22a74cd9fef3fca39d
Anuj Verma <anuj.verma@ebizontek.com>
2017-09-11 01:08:47 +0530

    deprecated warning removed in angular route

--- routes/angularapp.js
+++ routes/angularapp.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Angular App */
 router.get('*', function(req, res, next) {
-  res.sendfile( path.join( __dirname, "dist/index.html" ));
+  res.sendFile( path.join( __dirname, "dist/index.html" ));
 });
 
 module.exports = router;
