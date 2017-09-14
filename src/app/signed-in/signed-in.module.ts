051a4c7fd0c528e0e9346c68a5ec9b46223e1a7b
Anuj Verma <anuj.verma@ebizontek.com>
2017-09-14 20:45:10 +0530

    header and sidebar added

--- src/app/signed-in/signed-in.module.ts
+++ src/app/signed-in/signed-in.module.ts
@@ -4,16 +4,23 @@ import { FormsModule } from '@angular/forms';
 
 import { SignedInRoutingModule } from './signed-in-routing.module';
 
+import { MaterialModule } from '../shared/material.module';
+
 import { SignedInComponent } from './signed-in.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SignedInRoutingModule,
-    FormsModule
+    FormsModule,
+    MaterialModule
   ],
   declarations: [
-    SignedInComponent
+    SignedInComponent,
+    HeaderComponent,
+    SidebarComponent
   ]
 })
 export class SignedInModule { }
