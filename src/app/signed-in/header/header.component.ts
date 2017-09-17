72aaa04ae157eaadaf394a15d76dd539547a02d7
Anuj Verma <anuj.verma@ebizontek.com>
2017-09-18 01:28:27 +0530

    header changed

--- src/app/signed-in/header/header.component.ts
+++ src/app/signed-in/header/header.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pi-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
   constructor() { }
 
-  ngOnInit() {
+  signout() {
+    localStorage.removeItem( 'isPiconnectLoggedIn');
   }
-
 }
