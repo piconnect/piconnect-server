53ebe0d7cd5093bbaa024adf59c6aaddd9330242
Anuj Verma <anuj.verma@ebizontek.com>
2017-09-18 15:05:06 +0530

    add some styles

--- src/app/signed-in/header/header.component.ts
+++ src/app/signed-in/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pi-header',
@@ -7,9 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   signout() {
     localStorage.removeItem( 'isPiconnectLoggedIn');
+    this.router.navigate(['login']);
   }
 }
